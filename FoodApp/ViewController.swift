a8608c7fa9dbc1fdcb63f725cc0cce567f6e5b0d
Rguo18 <rg52@rice.edu>
2020-02-03 16:48:11 -0600

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by macpro on 2020/2/3.
+//  Copyright © 2020 Joyce Guo. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
